aeb96e52c4a05915e0066a10dc77b755abd433c3
Bien Gilzon Lara <bien.gilzon.c.lara@accenture.com>
2026-05-20 07:50:35 +0000

    30% done

--- webapp/controller/JSONBinding.controller.js
+++ webapp/controller/JSONBinding.controller.js
@@ -1,10 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("sapips.training.fy26q3sapui5basictrainingexercise4jsonbindingbien.controller.JSONBinding", {
         onInit() {
+            const oModel = new JSONModel( {
+                "EID": "bien.gilzon.c.lara",
+                "Enabled": true,
+                "Address": {
+                    "Street": "123 Main St",
+                    "Zip": "90210",
+                    "City": "Beverly Hills",
+                    "Country": "USA"
+                },
+                "SalesAmount": 12345.67,
+                "CurrencyCode": "USD"
+            } );
+
+            this.getView().setModel(oModel);    // default model
         }
     });
 });
